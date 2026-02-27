3a491ef51019adb5e16d58e06513474ee9d317f2
Vinoth-46 <vinothuser7@gmail.com>
2026-02-27 13:56:06 +0530

    security: remove exposed google API key from source control

---
+++ lib/secrets.dart
@@ -0,0 +1,4 @@
+class Secrets {
+  // TODO: Rotate this key in Google Cloud Console since it was exposed to GitHub.
+  static const String youtubeApiKey = 'AIzaSyCKKQYGUGiZz6xI7skyLBJL7npLrQICMIE';
+}
--- lib/services/youtube_music_service.dart
+++ lib/services/youtube_music_service.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import '../models/stream_song_model.dart';
+import '../secrets.dart';
 
 /// Quality levels for streaming based on network conditions
 enum StreamingQuality {
@@ -17,7 +18,7 @@ enum StreamingQuality {
 class YouTubeMusicService {
   final YoutubeExplode _yt = YoutubeExplode();
   StreamingQuality _preferredQuality = StreamingQuality.high;
-  static const String _youtubeApiKey = 'AIzaSyCKKQYGUGiZz6xI7skyLBJL7npLrQICMIE';
+  static const String _youtubeApiKey = Secrets.youtubeApiKey;
   
   /// Set preferred streaming quality
   void setQuality(StreamingQuality quality) {
